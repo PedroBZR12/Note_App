cecad5a3d5103a14e94e5ecebf749ceb690ec5b8
PedroBZR <bzrpedro@gmail.com>
2025-12-05 23:11:45 -0300

    nova feature tarefas
    Nova feature desenvolvida: agora você pode criar tarefas em uma outra página do aplicativo, para facilitar! O que precisa ser feito ainda: ajuste da interface do usuário

--- Note_App/app/(tabs)/_layout.tsx
+++ Note_App/app/(tabs)/_layout.tsx
@@ -15,18 +15,19 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+        tabBarStyle: { backgroundColor: "white"
+      },}}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Notas',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'Tarefas',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
--- Note_App/app/(tabs)/explore.tsx
+++ Note_App/app/(tabs)/explore.tsx
@@ -1,112 +1,189 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Alert, Platform, Pressable, ScrollView, StyleSheet } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, Text, TextInput, TouchableOpacity, ImageBackground, Modal } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { Collapsible } from '@/components/ui/collapsible';
-import { ExternalLink } from '@/components/external-link';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Fonts } from '@/constants/theme';
+export default function ExploreScreen() {
 
-export default function TabTwoScreen() {
+
+  useEffect(()=>{
+    //quando inicializar o app, irá ler as tarefas salvas
+    (async () => {
+      try {
+        const tarefas = await AsyncStorage.getItem('tarefas');
+        if(tarefas == null) {
+          setTarefa([]);
+        }else{
+          setTarefa(JSON.parse(tarefas));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  },[])
+
+
+
+  const image = require('../../assets/images/bg.jpg')
+  const [tarefa, setTarefa] = useState<any[]>([
+    {
+      id: 1,
+      nome: 'Estou usando o aplicativo de tarefas!',
+    },
+
+  ]);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [novaTarefaNome, setNovaTarefaNome] = useState('');
+
+  function deletarTarefa(id: number){
+    let newTarefa = tarefa.filter(function(val){
+      return val.id != id;
+    });
+    setTarefa(newTarefa);
+    alert ('Tarefa deletada com sucesso!');
+    
+    (async () => {
+      try {
+        await AsyncStorage.setItem('tarefas', JSON.stringify(newTarefa));
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }
+    function adicionarTarefa(nome: string){
+      let newId = tarefa.length + 1;
+      let novaTarefa = {id: newId, nome: nome};
+      setTarefa([...tarefa, novaTarefa]);
+      setModalVisible(!modalVisible), 
+      alert ('Tarefa adicionada com sucesso!');
+    }
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText
-          type="title"
-          style={{
-            fontFamily: Fonts.rounded,
+    <View style={{flex: 1, backgroundColor: 'white'}}>
+    <ScrollView style={styles.container}>
+      <StatusBar hidden={true}/>
+      <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
           }}>
-          Explore
-        </ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image
-          source={require('@/assets/images/react-logo.png')}
-          style={{ width: 100, height: 100, alignSelf: 'center' }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user&apos;s current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful{' '}
-          <ThemedText type="defaultSemiBold" style={{ fontFamily: Fonts.mono }}>
-            react-native-reanimated
-          </ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <TextInput onChangeText={setNovaTarefaNome} autoFocus={true} style={{width: 150, marginBottom: 20,borderColor: '#7f7b7bff', borderWidth: 1, borderRadius: 25}}></TextInput>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => adicionarTarefa(novaTarefaNome)}>
+                <Text style={styles.textStyle}>Adicionar Tarefa</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      <ImageBackground source={image} style={styles.image}>
+        <Text>Bem-vindo à tela de Tarefas!</Text>
+      </ImageBackground>
+      {
+        tarefa.map(function(val){
+          return(
+          <View key={val.id} style={styles.tarefaSingle}>
+            <View style={{flex: 1, width: '100%', paddingLeft: 10}}>
+              <Text>{val.nome}</Text>
+            </View>
+            <View style={{alignItems: 'flex-end', flex:1, paddingRight: 10,}}>
+              <TouchableOpacity onPress={() => deletarTarefa(val.id)}>
+                <AntDesign name="minus-circle" size={24} color="black" />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+        );})
+      }
+    </ScrollView>
+      <TouchableOpacity onPress={()=> setModalVisible(!modalVisible)} style={styles.btnAnotacao}>
+              <Text style={{fontSize: 30}}>+</Text>
+              </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  container: {
+    flex: 1,
+    height: '100%',
   },
-  titleContainer: {
+  image:{
+    width: '100%',
+    height: 100,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  tarefaSingle:{
+    marginTop: 20,
+    width: '100%',
+    height: 50,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
     flexDirection: 'row',
-    gap: 8,
+    paddingBottom: 10,
+  },
+    centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  btnAnotacao:{
+    position: 'absolute',
+    right: 20,
+    bottom: 20,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#a0a6acff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 5,
   },
 });
